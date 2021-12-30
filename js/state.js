ecbe60f5ac523be234359f02b133d42cbeb61506
kembo <n.kembo@gmail.com>
2021-12-30 15:00:02 +0900

    state を少し整理

--- js/state.js
+++ js/state.js
@@ -1,22 +1,24 @@
 "use strict";
-/**
- * 入力状態の状態遷移等のデータ型や必要な関数を定義
- */
+// 入力状態の状態遷移等のデータ型や必要な関数を定義
+/** 入力状態を表すクラス */
 class State {
     constructor(nextStates) {
+        /** 最終位置 */
         this.lastSpot = null;
         this.nextStates = nextStates;
     }
-    next(pos) {
-        return this.nextBySpot(detectSpot(pos));
-    }
-    nextBySpot(spot) {
+    /**
+     * Spot から状態遷移を判定する
+     * @param spot 所在地
+     * @returns 次の状態
+     */
+    _nextBySpot(spot) {
         if (spot === this.lastSpot) {
             return this;
         }
         const nextSt = this.nextStates[spot];
         if (nextSt === undefined) {
-            return State.defaultState;
+            return this.release();
         }
         if (nextSt === null) {
             this.lastSpot = spot;
@@ -24,17 +26,36 @@ class State {
         }
         return nextSt.comming(this, spot);
     }
+    /**
+     * 座標から状態遷移を判定する
+     * @param pos 座標
+     * @returns 次の状態
+     */
+    next(pos) {
+        return this._nextBySpot(detectSpot(pos));
+    }
+    /**
+     * 指を離した際の挙動
+     * @returns 次の状態
+     */
     release() {
         return State.defaultState.comming();
     }
+    /**
+     * この状態に遷移してきた時の挙動
+     * @param prev 呼び出し元の State
+     * @param spot 現在地
+     * @returns 自分自身
+     */
     comming(prev, spot) {
         this.lastSpot = spot;
         return this;
     }
 }
+/** 入力前の待機状態 */
 class PreTouchState extends State {
     next(pos) {
-        return this.nextBySpot(detectSpot(pos, true));
+        return this._nextBySpot(detectSpot(pos, true));
     }
     comming(prev) {
         return this;
--- src/state.ts
+++ src/state.ts
@@ -1,26 +1,36 @@
-/**
- * 入力状態の状態遷移等のデータ型や必要な関数を定義
- */
+// 入力状態の状態遷移等のデータ型や必要な関数を定義
 
+/**
+ * State の連なりを示す辞書
+ * 遷移させない範囲を null とし、その他の未定義の空欄は範囲外として指を離した時と同じ挙動になる。
+ */
 type StateChain = { [key in Spot]?: Nullable<State> };
+
+
+/** 入力状態を表すクラス */
 abstract class State {
-  lastSpot: Nullable<Spot> = null;
-  protected nextStates: StateChain;
+  /** 開始状態 */
   static defaultState: PreTouchState;
+  /** 表示用オブジェクト */
   static display: DisplayTable;
+  /** 最終位置 */
+  lastSpot: Nullable<Spot> = null;
+  /** 次の状態遷移を示すための辞書オブジェクト */
+  protected nextStates: StateChain;
 
   constructor(nextStates: StateChain) {
     this.nextStates = nextStates;
   }
 
-  public next(pos: Vector2): State {
-    return this.nextBySpot(detectSpot(pos));
-  }
-
-  protected nextBySpot(spot: Spot): State {
+  /**
+   * Spot から状態遷移を判定する
+   * @param spot 所在地
+   * @returns 次の状態
+   */
+  protected _nextBySpot(spot: Spot): State {
     if (spot === this.lastSpot) { return this; }
     const nextSt = this.nextStates[spot];
-    if (nextSt === undefined) { return State.defaultState; }
+    if (nextSt === undefined) { return this.release(); }
     if (nextSt === null) {
       this.lastSpot = spot;
       return this;
@@ -28,19 +38,40 @@ abstract class State {
     return nextSt.comming(this, spot);
   }
 
+  /**
+   * 座標から状態遷移を判定する
+   * @param pos 座標
+   * @returns 次の状態
+   */
+  public next(pos: Vector2): State {
+    return this._nextBySpot(detectSpot(pos));
+  }
+
+  /**
+   * 指を離した際の挙動
+   * @returns 次の状態
+   */
   public release(): PreTouchState {
     return State.defaultState.comming();
   }
 
+  /**
+   * この状態に遷移してきた時の挙動
+   * @param prev 呼び出し元の State
+   * @param spot 現在地
+   * @returns 自分自身
+   */
   public comming(prev: State, spot: Spot): typeof this {
     this.lastSpot = spot;
     return this;
   }
 }
 
+
+/** 入力前の待機状態 */
 class PreTouchState extends State {
   public next(pos: Vector2): State {
-    return this.nextBySpot(detectSpot(pos, true));
+    return this._nextBySpot(detectSpot(pos, true));
   }
 
   public comming(prev?: State): typeof this {
